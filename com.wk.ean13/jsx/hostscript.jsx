6c508a9ea4f0fefdb85baa011add94b3447b97ed
Marat Shagiev <m_js@bk.ru>
2016-09-03 01:07:55 +0300

    init porject from version 2.2.4

---
+++ com.wk.ean13/jsx/hostscript.jsx
@@ -0,0 +1,299 @@
+//encoding table: L R G; R = _mirror ( L ); G = _reverse ( _mirror ( L ) )
+
+function makeEan13 (o) {
+
+  var code       = o.str,
+      led        = o.chLed,
+      fntName    = o.fontName,
+      ch_center  = o.chCenter,
+      ch_select  = o.chSelect,
+      chNewLay   = o.chNewLay,
+      layNameVal = o.layNameVal,
+      reduceVal  = o.reduceVal,
+      chAddBg    = o.chAddBg;
+
+  if (code.length < 13) return;
+  if (activeDocument.activeLayer.visible == false) return;
+  if (activeDocument.activeLayer.locked == true) return;
+  if (documents.length == 0) return;
+
+  activeDocument.rulerOrigin = [0, activeDocument.height]; // Set Zero point ruler on Document
+
+  var encodeString,
+      symbol,
+      col          = new CMYKColor (),
+      MM_TO_PT     = 2.834645669,
+      PT_TO_MM     = 0.352777778,
+      structure    = [
+        'LLLLLL', 'LLGLGG', 'LLGGLG', 'LLGGGL', 'LGLLGG', 'LGGLLG', 'LGGGLL', 'LGLGLG', 'LGLGGL', 'LGGLGL'],
+      encodeDigits = [
+        '0001101', '0011001', '0010011', '0111101', '0100011', '0110001', '0101111', '0111011', '0110111',
+        '0001011'],
+      BAR_H        = 22.85,
+      BAR_SEP_H    = 24.5;
+
+  reduceVal = parseFloat (reduceVal) * MM_TO_PT;
+
+  col.black = 100;
+
+  encodeString = _makeEncodeStr (code);
+  symbol       = _makeBars (encodeString);
+  _makeDigits (code, symbol);
+
+  if (chAddBg == true) {
+    var rect = _makeRect (symbol.top, -3.34 * MM_TO_PT, 38 * MM_TO_PT, symbol.height, symbol, 0, new CMYKColor ());
+    rect.move (symbol, ElementPlacement.PLACEATEND);
+    rect.resize (100, 101, true, true, true, true, undefined, Transformation.CENTER);
+    rect.stroked = false;
+  }
+
+  if (ch_center == true) {
+    executeMenuCommand ('deselectall');
+    symbol.selected = true;
+    cut ();
+    paste ();
+    if (ch_select == false) {
+      executeMenuCommand ('deselectall');
+    }
+  }
+  if (ch_select == true && ch_center == false) {
+    executeMenuCommand ('deselectall');
+    symbol.selected = true;
+  }
+
+  function _makeDigits (code, digGroup) {
+    var digGr        = digGroup.groupItems.add ();
+    var startTop     = -(BAR_H + (BAR_SEP_H - BAR_H) / 3) * MM_TO_PT,
+        realTop      = startTop;
+    var left_fr0     = -3 * MM_TO_PT;
+    var startFntSize = 11,
+        realFntSize  = startFntSize,
+        fntH         = 6.8,
+        middTxtW     = 38.211,
+        left_fr1     = 3.74,
+        left_fr2     = 46.772,
+        left_fr3     = 89.924;
+
+    // this function lounch (o.tune == true) is changing realTop and realFntSize values
+    var digFr_1 = __addDig ({
+      cont: code.slice (1, 7), leftPnt: left_fr1, justify: 'center', tune: true
+    });
+    var digFr_2 = __addDig ({
+      cont: code.slice (7), leftPnt: left_fr2, justify: 'center', tune: false
+    });
+    var digFr_0 = __addDig ({
+      cont: code.slice (0, 1), leftPnt: left_fr0, justify: 'left', tune: false
+    });
+    if (led == true) {
+      var digFr_3 = __addDig ({
+        cont: '>', leftPnt: left_fr3, justify: 'right', tune: false
+      });
+    }
+
+    function __addDig (o /*o.cont, o.leftPnt, o.justify, o.tune*/) {
+      var pntTxt = digGr.textFrames.add ();
+
+      pntTxt.textRange.size                          = realFntSize;
+      pntTxt.contents                                = o.cont;
+      pntTxt.textRange.characterAttributes.fillColor = col;
+
+      try {
+        pntTxt.textRange.characterAttributes.textFont = textFonts.getByName (fntName);
+      } catch (e) {
+        pntTxt.textRange.characterAttributes.textFont = textFonts[0];
+      }
+
+      switch (o.justify) {
+        case 'left_fr0':
+          // !!! Justification.LEFT is not working
+          pntTxt.textRange.paragraphAttributes.justification = Justification.FULLJUSTIFYLASTLINELEFT;
+          break;
+        case 'right':
+          pntTxt.textRange.paragraphAttributes.justification = Justification.RIGHT;
+          break;
+        case 'center':
+          pntTxt.textRange.paragraphAttributes.justification = Justification.CENTER;
+          break;
+        default:
+          break;
+      }
+
+      if (o.justify == 'center') {
+        pntTxt.left = o.leftPnt + (middTxtW - pntTxt.width) / 2;
+      } else {
+        pntTxt.left = o.leftPnt;
+      }
+
+      if (o.tune == true) {
+        while (calcCharSize (pntTxt).h > fntH) {
+          realFntSize -= 0.2;
+          pntTxt.textRange.size = realFntSize;
+        }
+        realTop = startTop + calcCharSize (pntTxt).top;
+      }
+
+      pntTxt.top = realTop;
+
+      return pntTxt;
+
+    }
+
+  }
+
+  function _makeBars (str) {
+    var newLay,
+        codeGroup,
+        barsGroup;
+
+    if (chNewLay == true) {
+      newLay      = activeDocument.layers.add ();
+      newLay.name = layNameVal;
+      codeGroup   = newLay.groupItems.add ();
+      barsGroup   = codeGroup.groupItems.add ();
+    } else {
+      codeGroup = activeDocument.activeLayer.groupItems.add ();
+      barsGroup = codeGroup.groupItems.add ();
+    }
+
+    var x                                    = .33 * MM_TO_PT,
+        top = 0, left = 0, width = 0, height = BAR_H * MM_TO_PT;
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] == '0') {
+        if (width) {
+          str [i - 1] == '7' ? height = BAR_SEP_H * MM_TO_PT : height = BAR_H * MM_TO_PT;
+          _makeRect (top, left, width, height, barsGroup, reduceVal, col);
+          left += width + x;
+          width = 0;
+          continue;
+        }
+        left += x;
+        continue;
+      }
+      if (str [i] == '1' || str [i] == '7') {
+        width += x;
+      }
+    }
+    if (width) {
+      height = BAR_SEP_H * MM_TO_PT;
+      _makeRect (top, left, width, height, barsGroup, reduceVal, col);
+    }
+    return codeGroup;
+  }
+
+  function _makeEncodeStr (code) {
+    var resultStr = '707';
+    var structStr = structure [+code [0]];
+    var codeStart = code.slice (1, 7);
+    for (var i = 0; i < codeStart.length; i++) {
+      switch (structStr [i]) {
+        case 'L':
+          resultStr += encodeDigits [codeStart [i]];
+          break;
+        case 'G':
+          resultStr += _reverse (_mirror (encodeDigits [codeStart [i]]));
+          break;
+        default:
+          break;
+      }
+    }
+    resultStr += '07070';
+    var codeEnd = code.slice (7);
+    for (var j = 0; j < codeEnd.length; j++) {
+      resultStr += _mirror (encodeDigits[codeEnd[j]])
+    }
+    resultStr += '707';
+    return resultStr;
+  }
+
+  function _makeRect (top, left, width, height, container, reduce, col) {
+    container      = container || activeDocument.activeLayer;
+    var rect       = container.pathItems.rectangle (top, left, width + reduce, height);
+    rect.stroked   = false;
+    rect.fillColor = col;
+
+    return rect;
+  }
+
+  function _reverse (str) {
+    return str.split ('').reverse ().join ('');
+  }
+
+  function _mirror (str) {
+    var mirStr = '';
+    for (var i = 0; i < str.length; i++) {
+      str[i] == '1' ? mirStr += '0' : mirStr += '1';
+    }
+    return mirStr;
+  }
+
+  return encodeString;
+}
+
+function getFonts (chr) {
+  var font = {};
+  for (var i = 0; i < textFonts.length; i++) {
+    font[i] = {
+      family: textFonts[i].family,
+      style:  textFonts[i].style,
+      name:   textFonts[i].name
+    }
+  }
+  return JSON.stringify (font);
+}
+
+function killCEP () {
+  /**
+   * make bat-file that kill all system processes CEPHTMLEngine.exe
+   */
+  _execFile (
+    Folder.temp.absoluteURI + '/' + 'taskkil.bat',
+    'taskkill /IM CEPHTMLEngine.exe /f'
+  );
+  /**
+   * make new file by full path, write to disk with some file contenr, execute file
+   *
+   * @param {String} filePath - FULL path (include file-extension)
+   * @param {String} fileContent - content to new file
+   */
+  function _execFile (filePath, fileContent) {
+    var f = new File (filePath);
+    f.open ('e');
+    f.write (fileContent);
+    f.close ();
+    f.execute ();
+  }
+}
+
+function showAppVers () {
+  alert (app.version);
+}
+
+/**
+ * calculate top, bottom spasing and the real height of the capital characters
+ *
+ * @param {TextFrameItem} frame - object of the TextFrameItem class
+ * @return {Object} fntMeas - result object {h, top, bot, w}
+ */
+function calcCharSize (frame) {
+  var txt     = activeDocument.activeLayer.textFrames.add (),
+      fullH,
+      fntMeas = {};
+
+  txt.contents                               = frame.contents;
+  // txt.contents                               = 'C';
+  txt.textRange.characterAttributes.textFont = frame.textRange.characterAttributes.textFont;
+  txt.textRange.characterAttributes.size     = frame.textRange.characterAttributes.size;
+
+  var txtCurv = (txt.duplicate ()).createOutline ();
+
+  fullH       = txt.height;
+  fntMeas.h   = txtCurv.height;
+  fntMeas.top = Math.abs (txt.position[1] - txtCurv.position[1]);
+  fntMeas.bot = (fullH - fntMeas.h - fntMeas.top);
+  fntMeas.w   = txtCurv.w;
+
+  txt.remove ();
+  txtCurv.remove ();
+
+  return fntMeas;
+}
